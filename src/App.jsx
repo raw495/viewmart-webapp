52649b0c0d13959d7a53abf2822f8f4dfd7f3f2b
raw495 <raskol_fff@mail.ru>
2025-06-17 17:39:33 +0300

    Обновил дизайн заставки: VM по центру

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export default function App() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-300">
-      <div className="text-white text-5xl font-bold tracking-wide">VM</div>
+    <div className="flex items-center justify-center h-screen bg-blue-200">
+      <div className="text-white text-6xl font-extrabold tracking-wide">VM</div>
     </div>
   );
 }
